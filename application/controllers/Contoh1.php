2b7854ac68dc7ed852a40bfd9ed432786b3924c4
wisnupamungkas12 <wisnugrab1208@gmail.com>
2023-04-06 12:40:08 +0700

    Mengerjakan tugas pertemuan 3 dan 4

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -4,9 +4,9 @@ class Contoh1 extends CI_Controller
   public function index()
  {
    echo "<h1>Perkenalkan</h1>";
-   echo"Nama saya Angga Syahputra
-   Saya tingga di daerah Bogor
+   echo"Nama saya Wisnu Pamungkas
+   Saya tingga di daerah Pasar Rebo Jakarta Timur
    olahraga yang saya sukai adalah
-   Basket";
+   Berenang dan Futsal";
  }
 }
